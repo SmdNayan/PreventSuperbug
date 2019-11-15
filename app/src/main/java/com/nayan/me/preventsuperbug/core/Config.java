b67a3e075743d6acef8182598f7bf1fc89b45d81
Rashed Khan Arif <arifkhan0713@gmail.com>
2019-11-15 20:11:13 +0600

    up

--- app/src/main/java/com/nayan/me/preventsuperbug/core/Config.java
+++ app/src/main/java/com/nayan/me/preventsuperbug/core/Config.java
@@ -1,5 +1,5 @@
 package com.nayan.me.preventsuperbug.core;
 
 public class Config {
-    public static final String BASE_URL = "http://192.168.1.8:8080/";
+    public static final String BASE_URL = "http://192.168.1.3:8080/";
 }
